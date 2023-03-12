39b24df203ab61a5fadf81deefa6f51ca70e15c5
Ivan Tarasiuk <viix345@gmail.com>
2023-03-12 17:07:50 +0100

    lesson3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const arr =[
+  {
+  name:'Мужские кроссовки Nike Sneaker1',
+  price: 12999,
+  imageUrl:'/img/sneakers/sneak1.webp'
+},
+ {name:'Мужские кроссовки Nike Sneaker2', 
+ price:9999,
+ imageUrl:'/img/sneakers/sneak2.webp'
+},{name:'Мужские кроссовки Nike Sneaker3', 
+price:6800,
+imageUrl:'/img/sneakers/sneak3.jpg'
+},{name:'Мужские кроссовки Nike Sneaker4', 
+price:1200,
+imageUrl:'/img/sneakers/sneak4.webp'
+}]
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -17,7 +34,10 @@ function App() {
           </div>
         </div>
         <div className="d-flex">
-          <Card/>
+          {arr.map(el=>(
+            <Card key={el.name} {...el} onClick={()=>console.log(arr)}/>
+          ))
+          }      
           
         </div>
       </div>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,22 +1,27 @@
-function Card() {
+import styles from "./Card.module.scss";
+
+console.log(styles);
+
+function Card(props) {
+  const { name, price, imageUrl, onClick } = props;
+
+  // const onClickButton = () => {
+  //   alert(name);
+  // };
+
   return (
-    <div className="card">
+    <div className={styles.card}>
       <div className="favorite">
         <img width={14} height={14} src="/img/heart.svg" alt="Unliked" />
       </div>
-      <img
-        width={133}
-        height={112}
-        src="/img/sneakers/sneak1.webp"
-        alt="Sneakers"
-      />
-      <h5>Мужские кроссовки Nike Sneaker1</h5>
+      <img width={133} height={112} src={imageUrl} alt="Sneakers" />
+      <h5>{name}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12 999 rub.</b>
+          <b>{price} rub.</b>
         </div>
-        <button className="button">
+        <button className="button" onClick={() => onClick()}>
           <img width={11} height={11} src="/img/plus.svg" alt="plus" />
         </button>
       </div>
